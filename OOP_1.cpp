80dd729e215cbfd81175d78416bfbc617ab66df0
Bohdashga <https://github.com/Bohdashga/Lab_1.git>
2021-03-21 14:18:18 +0200

    First commit

---
+++ OOP_1.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+#define N 80
+
+int main()
+{
+	FILE* file;
+	char arr[N];
+	int i;
+	file = fopen("file.txt", "r");
+	while ((arr[i] = fgetc(file)) != EOF) {
+		if (arr[i] == '\n') {
+			arr[i] = '\0';
+			printf("%s\n", arr);
+			i= 0;
+		}
+		else i++;
+	}
+	arr[i] = '\0';
+	printf("%s\n", arr);
+
+	return 0;
+}
